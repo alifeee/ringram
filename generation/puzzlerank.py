93343e0847ee8d3589c71d8552f764a36976f281
alifeee <alifeeerenn@hotmail.com>
2024-06-15 22:00:12 +0100

    format python

--- generation/puzzlerank.py
+++ generation/puzzlerank.py
@@ -9,18 +9,25 @@ def by_unique_letters(puzzle: List[List[str]]) -> int:
     """Rank by number of unique letters"""
     return len(set(flatten_puzzle(puzzle)))
 
-def main(puzzles_wordy: List[str]):
+
+def main(puzzles_wordy: List[str], output: "" or "unique"):
     """main"""
     ranks = [by_unique_letters]
     puzzles = [words_to_puzzle_solved(words) for words in puzzles_wordy]
 
-    print(f"Ranking by unique letters")
+    if not output:
+        print(f"Ranking by unique letters")
     sorted_puzzles = sorted(puzzles, key=by_unique_letters, reverse=True)
-    for i, puzzle in enumerate(sorted_puzzles[:10]):
-        print(f"{i+1}. {by_unique_letters(puzzle)}")
-        print(", ".join(puzzle_solved_to_words(puzzle)))
+    if not output:
+        for i, puzzle in enumerate(sorted_puzzles[:10]):
+            print(f"{i+1}. {by_unique_letters(puzzle)}")
+            print(", ".join(puzzle_solved_to_words(puzzle)))
+    elif output == "unique":
+        for i, puzzle in enumerate(sorted_puzzles):
+            print(",".join(puzzle_solved_to_words(puzzle)))
 
-    print(f"Ranking by unique letters, skipping used words")
+    if not output:
+        print(f"Ranking by unique letters, skipping used words")
     used_words = set()
     sorted_puzzles = sorted(puzzles, key=by_unique_letters, reverse=True)
     puzzles_freq_filtered = []
@@ -30,8 +37,10 @@ def main(puzzles_wordy: List[str]):
             puzzles_freq_filtered.append(puzzle)
             used_words.update(words)
     for i, puzzle in enumerate(puzzles_freq_filtered[:10]):
-        print(f"{i+1}. {by_unique_letters(puzzle)}")
-        print(", ".join(puzzle_solved_to_words(puzzle)))
+        if not output:
+            print(f"{i+1}. {by_unique_letters(puzzle)}")
+        if not output:
+            print(", ".join(puzzle_solved_to_words(puzzle)))
 
 
 if __name__ == "__main__":
@@ -47,12 +56,18 @@ if __name__ == "__main__":
         "--input",
         help="Input file with words",
     )
+    parser.add_argument(
+        "-o",
+        "--output",
+        choices=["unique"],
+        help="type of metric to sort by and output puzzles",
+    )
     args = parser.parse_args()
     if args.input:
         words = []
         with open(args.input, "r", encoding="utf-8") as f:
             for line in f:
-                words.append(line.strip().split(",")) # csv
+                words.append(line.strip().split(","))  # csv
     else:
         words = [args.words]
-    main(words)
+    main(words, args.output)
